cce42ad4a5260136da857abe46b9cba5c7067c73
crismartin <cris.martinz.urjc@hotmail.es>
2019-09-04 23:23:06 +0200

    Se ha agregado bootstrap

--- ecg_reader/index.py
+++ ecg_reader/index.py
@@ -7,6 +7,7 @@ Created on Sun Jul 28 21:40:58 2019
 """
 
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 import ecg_factory as ecgf
@@ -16,8 +17,7 @@ from dash.dependencies import Input, Output
 
 import plotly.graph_objs as go
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 
 # Factoria de ECG
 ecgFactory = ecgf.ECGFactory()
@@ -35,28 +35,50 @@ ejeX = np.arange(0, len(ejeY), 1.0)/fs
 
 
                 
+############################## ##### #############################
+############################## FRONT #############################
+############################## ##### #############################
+                
 #Dropdown para elegir los leads del ECG
 optsLeads = [ {'value': i+1, 'label': 'Derivacion '+ str(i+1)} for i in range(nLeads)]
-print(optsLeads)
 
 #Objeto grafica
 ecg_trace = go.Scatter(x = ejeX, y = ejeY,
                     name = 'SF', mode='lines')
 
-layout = go.Layout(title = 'Representacion de ECG con formato ' + ecg.typeECG,
+print(optsLeads)
+layout = go.Layout(title = "Representacion de la Derivacion " + str(optsLeads[0]['value']),
                    hovermode = 'closest')
 
 
 fig = go.Figure(data = [ecg_trace], layout = layout)
 
-print("esto se hace!")
 
-#Creamos el Dash Layout
 
-app.layout = html.Div(children=[
-    html.H1(children='ECG App'),
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Link", href="#")),
+        dbc.DropdownMenu(
+            nav=True,
+            in_navbar=True,
+            label="Menu",
+            children=[
+                dbc.DropdownMenuItem("Entry 1"),
+                dbc.DropdownMenuItem("Entry 2"),
+                dbc.DropdownMenuItem(divider=True),
+                dbc.DropdownMenuItem("Entry 3"),
+            ],
+        ),
+    ],
+    brand="ECG App",
+    brand_href="#",
+    sticky="top",
+)
 
-             
+body = dbc.Container([
+        
+    html.H1(children='Formato ' + ecg.typeECG),
+
     #Agregamos la figura
     dcc.Graph(id='plot', figure=fig),
     
@@ -65,13 +87,19 @@ app.layout = html.Div(children=[
         html.Label("Elije una derivacion"),
         dcc.Dropdown(id = 'opt', 
                      options = optsLeads,
-                     value = optsLeads[0]['value'])
+                     value = optsLeads[0]['value'],
+                     clearable=False)
             ], style = {'width': '400px',
                         'fontSize' : '20px',
                         'padding-left' : '100px',
                         'display': 'inline-block'})
-    
 ])
+
+
+# Dash APP
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = html.Div([navbar, body])
     
 # Agregamos el callback para actualizar el dropdown
 @app.callback(Output('plot', 'figure'),
@@ -84,6 +112,8 @@ def update_figure(lead):
                     
     ecg_trace = go.Scatter(x = ejeX, y = ejeY,
                     name = 'SF', mode='lines')
+    layout = go.Layout(title = "Representacion de la Derivacion " + str(lead),
+                   hovermode = 'closest')
     fig = go.Figure(data = [ecg_trace], layout = layout)
     return fig
 
