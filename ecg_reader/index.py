f6f845a78dae92c3398befb971c01c351851c8db
crismartin <cris.martinz.urjc@hotmail.es>
2019-11-08 00:02:39 +0100

    Subida de fichero
    Se ha puesto un esqueleto de ventana modal para la subida de ficheros a la app

--- ecg_reader/index.py
+++ ecg_reader/index.py
@@ -13,7 +13,7 @@ import dash_html_components as html
 import ecg_factory as ecgf
 import numpy as np
 
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 import plotly.graph_objs as go
 
@@ -24,7 +24,7 @@ ecgFactory = ecgf.ECGFactory()
         
 #FORMATO PHYSIONET: ./sample-data/100
 #FORMATO ISHNE:     ./matlab_ishne_code/ishne.ecg
-ecg = ecgFactory.create_ECG("./sample-data/100")
+ecg = ecgFactory.create_ECG("./matlab_ishne_code/ishne.ecg")
 signals = ecg.signal
 nLeads = ecg.header.nLeads
 fs = ecg.header.samplingRate
@@ -75,9 +75,52 @@ navbar = dbc.NavbarSimple(
     sticky="top",
 )
 
+formulario = dbc.Form([
+        
+    dbc.FormGroup([
+        dbc.Label("Url remota", html_for="url-rem-file"),
+        dbc.Input(type="url", id="url-rem-file", placeholder="Url del fichero en remoto"),
+    ]),
+    html.P([
+        html.Label("o")
+    ]),
+    dbc.FormGroup([
+        dcc.Upload([
+            'Drag and Drop or ',
+            html.A('Select a File')
+        ], style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center'
+        })         
+    ])
+])
+
 body = dbc.Container([
         
     html.H1(children='Formato ' + ecg.typeECG),
+    
+    
+    html.Div([
+        dbc.Button("Subir fichero", id="open"),
+        dbc.Modal(
+            [
+                dbc.ModalHeader("Cargar Fichero"),
+                dbc.ModalBody([
+                    formulario
+                ]),
+                dbc.ModalFooter(
+                    dbc.Button("Close", id="close", className="ml-auto")
+                ),
+            ],
+            id="modal",
+            size="lg"
+        ),
+    ]),
 
     #Agregamos la figura
     dcc.Graph(id='plot', figure=fig),
@@ -92,15 +135,29 @@ body = dbc.Container([
             ], style = {'width': '400px',
                         'fontSize' : '20px',
                         'padding-left' : '100px',
-                        'display': 'inline-block'})
+                        'display': 'inline-block'}),
+        
+   
 ])
+
+
+
 
 
 # Dash APP
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([navbar, body])
-    
+
+
+
+
+
+###############################################################################
+############################### CALLBACKS #####################################
+###############################################################################
+
+
 # Agregamos el callback para actualizar el dropdown
 @app.callback(Output('plot', 'figure'),
              [Input('opt', 'value')])
@@ -117,5 +174,19 @@ def update_figure(lead):
     fig = go.Figure(data = [ecg_trace], layout = layout)
     return fig
 
+
+@app.callback(
+    Output("modal", "is_open"),
+    [Input("open", "n_clicks"), Input("close", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+###############################################################################
+############################### RUNER APP #####################################
+###############################################################################
 if __name__ == '__main__':
     app.run_server(debug=True)
